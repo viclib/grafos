9b8bd96f96bf208b7987237aab1fdcf4bbec3289
viclib <vh@viclib.com>
2014-09-16 18:42:17 -0300

    coisas

--- grafos.js
+++ grafos.js
@@ -1,3 +1,11 @@
+var nodejs = typeof this.window === "undefined";
+var browser = !nodejs;
+var marks = {};
+var mark = function(name){ marks[name] = Date.now(); };
+var stop = function(name){ console.log(">> "+name+" took "+((Date.now()-marks[name])/1000)+"s"); };
+var fs = require("fs");
+
+
 function Matrix(w,h){
     this.w = w;
     this.h = h;
@@ -27,44 +35,41 @@ Graph.prototype.dfs = function(n){
         };
     };
 };
-Graph.prototype.bfs = function(n,next){
-    for (var i = this.marked.length - 1; i >= 0; i--)
-        this.marked[i] = 0;
+
+Graph.prototype.bfs = function(n,debug){
+    var self = this;
+    for (var i = self.marked.length - 1; i >= 0; i--)
+        self.marked[i] = 0;
     var stack = [];
     var index = 0;
-    this.marked[n-1] = 1;
-    this.parent[n-1] = 0;
-    this.level[n-1] = 0;
-    stack.push(n)
-    while(index < stack.length){
+    self.parent[n-1] = 0;
+    self.level[n-1] = 0;
+    self.marked[n-1] = 1;
+    stack.push(n);
+    for (var index = 0; index < stack.length; ++index){
         var node = stack[index];
-        var neigs = this.neighbors(node);
+        var neigs = self.neighbors(node);
+        console.log("..",neigs);
+        if (debug) console.log(
+            "Estou no "+node+" (indice "+index+"/"+(stack.length-1)+")"
+                + "  \n -- a stack é: "    + JSON.stringify(stack)
+                + "  \n -- meus viz são: " + JSON.stringify(neigs)
+                + "  \n -- meu nivel é: "  + self.level[node-1]
+                + "  \n -- meu pai é: "    + self.parent[node-1]
+                + "\n");
         neigs.sort(function(a,b){return a-b});
-        console.log(
-            "Estou no "
-                +node
-                +" (indice "+index+"/"+stack.length+")"
-                +"  \n -- a stack é: "+JSON.stringify(stack)
-                +"  \n -- meus viz são: "+JSON.stringify(neigs)
-                +"  \n -- meu nivel é: "+this.level[node-1]
-                +"  \n -- meu pai é: "+this.parent[node-1]
-                + "\n"
-                );
-        ++index;
         for (var i = 0, l = neigs.length; i<l; ++i){
             var neig = neigs[i];
-            if (!this.marked[neig-1]) {
-                this.parent[neig-1] = node;
-                this.level[neig-1] = this.level[node-1] + 1;
-                this.marked[neig-1] = 1;
+            if (!self.marked[neig-1]) {
+                self.parent[neig-1] = node;
+                self.level[neig-1] = self.level[node-1] + 1;
+                self.marked[neig-1] = 1;
                 stack.push(neig);
             };
         };
-        if (next) {
-            
-        };
     };
 };
+
 
 function ArrayGraph(n){
     Graph.call(this,n);
@@ -85,7 +90,7 @@ ArrayGraph.prototype.hasEdge = function(x,y){
     return false;
 };
 ArrayGraph.prototype.neighbors = function(n){
-    return this.array[n];
+    return this.array[n-1];
 };
 
 
@@ -111,25 +116,44 @@ MatrixGraph.prototype.neighbors = function(n){
     return neighbors;
 };
 
-var mg = new MatrixGraph(10);
-mg.addEdge(1,2);
-mg.addEdge(2,3);
-mg.addEdge(3,4);
-mg.addEdge(1,5);
-mg.addEdge(1,8);
-mg.addEdge(8,7);
-mg.addEdge(8,6);
-mg.addEdge(4,5);
-mg.addEdge(5,6);
-mg.addEdge(5,10);
-mg.addEdge(9,10);
-//mg.dfs(1);
-mg.bfs(1);
+var buildGraphFromFile = function(file,Graph,callback){
+    var graph;
+    var fileStream = fs.createReadStream(file,{encoding:"utf8"});
+    var file = "";
+    fileStream.on("data",function(chunk){
+        var newLineIndex;
+        file = file + chunk;
+        while ((newLineIndex = file.indexOf("\n")) !== -1){
+            if (!graph){
+                graph = new Graph(Number(file.slice(0,newLineIndex)));
+                console.log("Created graph with size = "+graph.size);
+            } else {
+                var spaceIndex = file.indexOf(" ");
+                graph.addEdge(
+                    Number(file.slice(0,spaceIndex)),
+                    Number(file.slice(spaceIndex+1,newLineIndex)));
+            };
+            file = file.slice(newLineIndex+1);
+        };
+    });
+    mark("rf");
+    fileStream.on("end",function(){ 
+        callback(graph);
+    });
+};
+
+var file = "./data/as_graph.txt";
+var file = "./data/subdblp.txt";
+g = buildGraphFromFile(file,MatrixGraph,function(graph){
+    console.log("Loaded!");
+    //console.log(JSON.stringify(graph));
+});
 
 
-function render(graph){
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
+function render(params){
+    var graph = params.graph;
+    var canvas = params.canvas;
+    var ctx = params.ctx;
     var x = window.innerWidth * 0.5;
     var y = window.innerHeight * 0.5;
     //canvas.style.border = "2px solid black";
@@ -138,6 +162,7 @@ function render(graph){
     var rad = 150;
     ctx.fillStyle = "#FFFFFF";
     ctx.strokeStyle = "#000000";
+    ctx.clearRect(0,0,canvas.widht,canvas.height);
     function pos(node){
         return {
             x:x + Math.cos(Math.PI*2/graph.size*node) * rad, 
@@ -145,17 +170,15 @@ function render(graph){
     for (var i=1,l=graph.size; i<=l; ++i){
         graph.neighbors(i).map(function(n){
             ctx.beginPath();
-            ctx.moveTo(
-                pos(i).x, 
-                pos(i).y);
-            ctx.lineTo(
-                pos(n).x, 
-                pos(n).y);
+            ctx.moveTo(pos(i).x,pos(i).y);
+            ctx.lineTo(pos(n).x,pos(n).y);
             //console.log(pos(i).x, pos(i).y, pos(n).x, pos(n).y);
             ctx.stroke();
         });
     };
     for (var i=1,l=graph.size; i<=l; ++i){
+        ctx.fillStyle = graph.marked[i-1] ? "#AAAAAA" : "#FFFFFF";
+        ctx.fillStyle = "#FFFFFF";
         ctx.beginPath();
         ctx.arc(pos(i).x, pos(i).y, 26, 0, 2*Math.PI);
         ctx.fill();
@@ -166,13 +189,36 @@ function render(graph){
         ctx.font = "20px Georgia";
         ctx.fillText(i, pos(i).x - 4, pos(i).y + 4);
     };
-    return canvas;
 };
 
-if (typeof window !== "undefined"){
-    var renderElement = render(mg); 
-    document.body.appendChild(renderElement);
-};
+
+
+
+
+
+//var g = new ArrayGraph(10);
+//g.addEdge(1,2);
+//g.addEdge(2,3);
+//g.addEdge(3,4);
+//g.addEdge(1,5);
+//g.addEdge(1,8);
+//g.addEdge(8,7);
+//g.addEdge(8,6);
+//g.addEdge(4,5);
+//g.addEdge(5,6);
+//g.addEdge(5,10);
+//g.addEdge(9,10);
+//g.bfs(1,true);
+//if (browser){
+    //var canvas = document.createElement("canvas");
+    //var canvasCtx = canvas.getContext("2d");
+    //document.body.appendChild(canvas);
+    //render({graph:g, ctx:canvasCtx, canvas:canvas});
+    //setTimeout(function(){
+        //render({graph:g, canvas:canvas, ctx:canvasCtx}); 
+    //},1000);
+//}; 
+
 
 
 
